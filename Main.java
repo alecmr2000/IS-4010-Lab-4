82caa56d99e090aceda3002d73040dd9902ed16c
alecmr2000 <alecmr2000@gmail.com>
2021-02-08 00:37:13 +0000

    Added the countdown.

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 import java.util.Random;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Random r = new Random();
+    //Above is creating a new random object.
+
+    int r_randomNum = r.nextInt(51);
+    //Above is creating a place to store the random number.
+
+    System.out.println("The Random Number is: "+r_randomNum);
+    //Above is stating what the random number is.
+
+    while (r_randomNum >=0){
+      System.out.println(r_randomNum);
+      r_randomNum--;
+    }
   }
 }
