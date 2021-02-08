6de256a5dae3fa8d1af7e05243ffea8cd13fffc9
alecmr2000 <alecmr2000@gmail.com>
2021-02-08 22:56:28 +0000

    Final Version.1

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/* 
+Lab 4
+Partners: Alec Rezelj and Chris Fenter
+Date: 02.08.2021
+*/
+
 import java.util.Random;
 class Main {
   public static void main(String[] args) {
@@ -11,9 +17,33 @@ class Main {
     System.out.println("The Random Number is: "+r_randomNum);
     //Above is stating what the random number is.
 
-    while (r_randomNum >=0){
-      System.out.println(r_randomNum);
-      r_randomNum--;
+    if (r_randomNum <= 5){
+      while (r_randomNum >=0){
+        System.out.println(r_randomNum);
+        r_randomNum--;
+      }
+      System.out.println("Ahoy mateys!");
+    }
+    else if (r_randomNum > 25 && r_randomNum <42){
+      while (r_randomNum >=0){
+        System.out.println(r_randomNum);
+        r_randomNum--;
+      }
+      System.out.println("Cannonball!");
+    }
+    else if (r_randomNum > 5 && r_randomNum <=25){
+      while (r_randomNum >=0){
+        System.out.println(r_randomNum);
+        r_randomNum--;
+      }
+      System.out.println("Blast Off!");
+    }
+    else if (r_randomNum >=42 && r_randomNum <=50){
+      while (r_randomNum >=0){
+        System.out.println(r_randomNum);
+        r_randomNum--;
+      }
+      System.out.println("Blast Off!");
     }
   }
 }
